190e27065b612f9ccee05caf9c1274c77bc3b785
Sayan <115384735+Khsayan@users.noreply.github.com>
2022-11-11 18:36:32 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings'
+import Profile from './components/Profile/Profile';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
+const App = (props) => {
+       return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs/*'
+                               element={<DialogsContainer /> } />
+                        <Route path='/profile'
+                               element={<Profile /> } />
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>);
+}
+
+export default App;
